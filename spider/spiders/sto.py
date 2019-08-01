5cfe7c6e811d15a3d4b230c04c9a0f34e1d47961
beyondhtx <beyondhtx@gmail.com>
2019-08-01 17:23:52 +0800

    mdf

--- spider/spiders/sto.py
+++ spider/spiders/sto.py
@@ -6,7 +6,7 @@ class StoSpider(scrapy.Spider):
     name = 'sto'
     allowed_domains = ['stackoverflow.com']
     start_urls = []
-    for i in range(1000):
+    for i in range(6000):
         start_urls.append('https://stackoverflow.com/questions/tagged/javascript?tab=votes&page=' + str(i+1) + '&pagesize=50')
     #start_urls = ['http://stackoverflow.com/questions?sort=votes']
 
@@ -20,5 +20,6 @@ class StoSpider(scrapy.Spider):
             'title': response.css('h1 a::text').extract()[0],
             # 'q_votes': response.css('.js-vote-count::text').extract()[0],
             'code': response.css('div.answer pre code::text').extract()[0],
-            'a_votes': response.css('.js-vote-count::text').extract()[1]
+            'a_votes': response.css('.js-vote-count::text').extract()[1],
+            'url': response.url
         }
